13f6da5fdded9fbd6e577b2188b058718048bfa4
Sensetivity <notfound.la2.asterios.tm@gmail.com>
2017-03-31 21:05:43 +0300

    fixed arrow animation when menu is open/close and label font bold

--- widgets/Menu.php
+++ widgets/Menu.php
@@ -25,8 +25,8 @@ class Menu extends \yii\widgets\Menu
     protected function renderItem($item)
     {
         if (isset($item['items'])) {
-            $labelTemplate = '<a href="{url}">{label} <i class="fa fa-angle-left pull-right"></i></a>';
-            $linkTemplate = '<a href="{url}">{icon} {label} <i class="fa fa-angle-left pull-right"></i></a>';
+            $labelTemplate = '<a href="{url}">{label} <i class="fa arrow"></i></a>';
+            $linkTemplate = '<a href="{url}">{icon} {label} <i class="fa arrow"></i></a>';
         } else {
             $labelTemplate = $this->labelTemplate;
             $linkTemplate = $this->linkTemplate;
@@ -47,7 +47,7 @@ class Menu extends \yii\widgets\Menu
         } else {
             $template = ArrayHelper::getValue($item, 'template', $labelTemplate);
             $replace = !empty($item['icon']) ? [
-                '{label}' => '<span>' . $item['label'] . '</span>',
+                '{label}' => '<span class="nav-label">' . $item['label'] . '</span>',
                 '{icon}' => '<i class="' . $item['icon'] . '"></i> '
             ] : [
                 '{label}' => '<span>' . $item['label'] . '</span>',
